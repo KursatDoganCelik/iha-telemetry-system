d435d5e47f038d068b0527d2a291762200622eb8
KursatDoganCelik <kursatdogancelik@gmail.com>
2025-08-27 08:52:04 +0300

    initial commit

---
+++ src/main/java/com/ihaproject/IhaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ihaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhaProjectApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(IhaProjectApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/ihaproject/IhaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhaProjectApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
